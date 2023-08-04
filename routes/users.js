906de575d94494eb24081d775206f089ba800fb6
Yllies <hadjyllies@gmail.com>
2023-08-04 12:18:13 +0200

    add latitude longitude

--- routes/users.js
+++ routes/users.js
@@ -7,7 +7,6 @@ const { checkBody } = require("../modules/checkBody");
 const bcrypt = require("bcrypt");
 const uid2 = require("uid2");
 
-<<<<<<< HEAD
 router.post("/signup", (req, res) => {
   if (
     !checkBody(req.body, [
@@ -18,20 +17,18 @@ router.post("/signup", (req, res) => {
       "name",
       "siret_siren",
       "address",
+      "longitude",
+      "latitude",
+      "latitudeDelta",
+      "longitudeDelta",
     ])
   ) {
     res.json({ result: false, error: "Missing or empty fields" });
-=======
-router.post('/signup', (req, res) => {
-  if (!checkBody(req.body, ['email','username','password','type','name','siret_siren','address','longitude',"latitude","latitudeDelta","longitudeDelta"])) {
-    res.json({ result: false, error: 'Missing or empty fields' });
->>>>>>> 43127b4a74066207254b7fa13c3c4b4e8815afd3
     return;
   }
 
   // Check if the user has not already been registered
   //on check via leur siret/siret
-<<<<<<< HEAD
   const tokenAPI = "e6b24e73-7c80-3ec5-b16d-358d9ab783f9";
 
   fetch(
@@ -42,38 +39,6 @@ router.post('/signup', (req, res) => {
         "Content-Type": "application/json",
         Authorization: `Bearer ${tokenAPI}`,
       },
-=======
-  User.findOne({siret_siren: req.body.siret_siren}).then(data => {
-    if (data === null) {
-      const hash = bcrypt.hashSync(req.body.password, 10);
-//pour les données qu'on a pas demandé lors de l'inscription, je les enregistre par defaut en null
-//par la suite bien on pourras les modifié via une route PUT
-      const newUser = new User({
-        email: req.body.email,
-        password: hash,
-        name: req.body.name,
-        username:req.body.username,
-        address: req.body.address,
-        siret_siren: req.body.siret_siren,
-        type: req.body.type,
-        description: null,
-        phone_number: null,
-        url_site: null,
-        logo: null,
-        longitude: req.body.longitude,
-        latitude:req.body.latitude,
-        longitudeDelta: req.body.longitudeDelta,
-        latitudeDelta:req.body.latitudeDelta,
-        token: uid2(32),
-      });
-      
-      newUser.save().then(newDoc => {
-        res.json({ result: true, token: newDoc.token });
-      });
-    } else {
-      // User already exists in database
-      res.json({ result: false, error: 'User already exists' });
->>>>>>> 43127b4a74066207254b7fa13c3c4b4e8815afd3
     }
   )
     .then((response) => response.json())
@@ -101,6 +66,10 @@ router.post('/signup', (req, res) => {
               phone_number: null,
               url_site: null,
               logo: null,
+              longitude: req.body.longitude,
+              latitude: req.body.latitude,
+              longitudeDelta: req.body.longitudeDelta,
+              latitudeDelta: req.body.latitudeDelta,
               token: uid2(32),
             });
 
@@ -134,6 +103,10 @@ router.post('/signup', (req, res) => {
               phone_number: null,
               url_site: null,
               logo: null,
+              longitude: req.body.longitude,
+              latitude: req.body.latitude,
+              longitudeDelta: req.body.longitudeDelta,
+              latitudeDelta: req.body.latitudeDelta,
               token: uid2(32),
             });
 
@@ -177,9 +150,8 @@ router.post("/signin", (req, res) => {
   );
 });
 
-<<<<<<< HEAD
-router.delete("/:userId", (req, res) => {
-  const userId = req.params.userId;
+router.delete("/", (req, res) => {
+  const token = req.body.token;
 
   User.deleteOne({ _id: userId }).then((data) => {
     console.log(data);
@@ -189,29 +161,28 @@ router.delete("/:userId", (req, res) => {
       res.json({ result: false, message: "User not found or already deleted" });
     }
   });
-=======
-
-
-router.delete("/", (req, res) => {
-  const token = req.body.token;
-
-  User.findOne({ token })
-    .then(user => {
-      if (!user) {
-        res.json({ result: false, error: 'User not found' });
-        return;
-      }
-      User.deleteOne({ token })
-        .then(data => {
-          console.log(data);
-          if (data.deletedCount >= 1) {
-            res.json({ result: true, message: 'User deleted' });
-          } else {
-            res.json({ result: false, message: 'User not found or already deleted' });
-          }
-        })
-    })
->>>>>>> 43127b4a74066207254b7fa13c3c4b4e8815afd3
 });
-    
+
+router.delete("/:userId", (req, res) => {
+  const userId = req.params.userId;
+
+  User.findOne({ token }).then((user) => {
+    if (!user) {
+      res.json({ result: false, error: "User not found" });
+      return;
+    }
+    User.deleteOne({ token }).then((data) => {
+      console.log(data);
+      if (data.deletedCount >= 1) {
+        res.json({ result: true, message: "User deleted" });
+      } else {
+        res.json({
+          result: false,
+          message: "User not found or already deleted",
+        });
+      }
+    });
+  });
+});
+
 module.exports = router;
