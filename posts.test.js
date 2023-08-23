c463e925eb3debc0c1cc0173be222d5c15b1e2e6
Wissem <wbousdira@gmail.com>
2023-08-23 16:36:17 +0200

    vercel

--- posts.test.js
+++ posts.test.js
@@ -17,3 +17,46 @@ mongoose
 
 app.use(express.json());
 app.use("/", router);
+
+describe('GET /posts/company', () => {
+  it('should retrieve all company posts', async () => {
+    const response = await request(app).get('/posts/company');
+    
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('posts');
+    expect(Array.isArray(response.body.posts)).toBe(true);
+
+  });
+
+  it('should handle errors if the route is not defined', async () => {
+    const response = await request(app).get('/posts/nonexistent');
+
+    expect(response.status).toBe(404);
+  });
+});
+
+describe('GET /charity', () => {
+  it('should retrieve all charity posts', async () => {
+    // Create mock data for the test
+    const mockPosts = [
+      { title: 'Post 1', description: 'Description 1', category: 'Category A' },
+      { title: 'Post 2', description: 'Description 2', category: 'Category B' },
+    ];
+
+    // Use jest.spyOn to mock the PostAssociation.find method
+    const findSpy = jest.spyOn(PostAssociation, 'find').mockResolvedValue(mockPosts);
+
+    const response = await request(app).get('/charity');
+
+    // Assertions
+    expect(response.status).toBe(200);
+    expect(response.body).toHaveProperty('posts', mockPosts);
+
+    // Restore the original implementation of PostAssociation.find
+    findSpy.mockRestore();
+  });
+
+  });
+s
+
+
